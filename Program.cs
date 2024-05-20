6918fcb476f8f782467f684985d4b8129b9ca5d7
Brennan <blfische92@gmail.com>
2024-05-20 18:16:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Assignment3._1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         MyMethods.IsYearLeap(2024);
+        }
+
+        static public class MyMethods
+        {
+            static public bool IsYearLeap(int myYear)
+            {
+                bool isYearLeap;
+
+                if (myYear%4 == 0)
+                {
+                    isYearLeap = true;
+                    Console.WriteLine(Convert.ToString(isYearLeap) + " "+ Convert.ToString(myYear) + " year is a leap year");
+                }
+                else
+                {
+                    isYearLeap = false;
+                    Console.WriteLine(Convert.ToString(isYearLeap) + "year is not a leap year");
+                }
+                return isYearLeap;
+            }
+        }
+    }
+}
